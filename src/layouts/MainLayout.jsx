d47334b148ddeb4bae6b9012aa0c500e051a788e
Tanjilul Islam <tanjilulislam37@gmail.com>
2025-10-26 21:34:36 +0600

    sticky scroll bar implemented

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -20,13 +20,13 @@ const MainLayout = () => {
       </header>
       <main className="w-11/12 mx-auto my-3 grid grid-cols-12 gap-5">
 
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-0 h-fit">
           <LeftAside/>
         </aside>
         <section className="main col-span-6">
           <Outlet />
         </section>
-       <aside className="col-span-3"> 
+       <aside className="col-span-3 sticky top-0 h-fit"> 
         <RightAside/>
        </aside>
       </main>
